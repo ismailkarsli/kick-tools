9040f1a57e1b22f178d4c3fd3011fef62f52e5be
İsmail Karslı <ismail@karsli.net>
2024-02-18 18:03:07 +0300

    update version

--- src/userscript.js
+++ src/userscript.js
@@ -6,6 +6,6 @@
 // @namespace https://github.com/ismailkarsli
 // @homepageURL https://github.com/ismailkarsli/kick-tools
 // @supportURL https://github.com/ismailkarsli/kick-tools/issues
-// @version 2.1.0
+// @version 2.1.1
 // @match https://*.kick.com/*
 // ==/UserScript==
